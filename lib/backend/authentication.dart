2fd2852fe1f4d47e08c0e311e3cbdbf6f75a4642
Sourish Mukherjee <56296940+Sourish-Mukherjee@users.noreply.github.com>
2021-07-13 21:46:53 +0530

    Added Google SignIn

--- lib/backend/authentication.dart
+++ lib/backend/authentication.dart
@@ -1,12 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class Authentication {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
   Future<String> signInUser(
       {@required String email, @required String password}) async {
-    final auth = FirebaseAuth.instance;
     try {
-      await auth.signInWithEmailAndPassword(email: email, password: password);
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
       return "";
     } on FirebaseAuthException catch (e) {
       return e.message;
@@ -15,13 +17,47 @@ class Authentication {
 
   Future<String> signUpUser(
       {@required String email, @required String password}) async {
-    final auth = FirebaseAuth.instance;
     try {
-      await auth.createUserWithEmailAndPassword(
+      await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
       return "";
     } on FirebaseAuthException catch (e) {
       return e.message;
     }
+  }
+
+  Future<String> signInWithGoogle() async {
+    final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleSignInAuthentication.accessToken,
+      idToken: googleSignInAuthentication.idToken,
+    );
+    try {
+      final UserCredential authResult =
+          await _auth.signInWithCredential(credential);
+      final User user = authResult.user;
+
+      if (user != null) {
+        assert(!user.isAnonymous);
+        assert(await user.getIdToken() != null);
+
+        final User currentUser = _auth.currentUser;
+        assert(user.uid == currentUser.uid);
+
+        print('signInWithGoogle succeeded: $user');
+
+        return "";
+      }
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+
+  Future<void> signOutGoogle() async {
+    await googleSignIn.signOut();
+    print("User Signed Out");
   }
 }
--- lib/screens/signin/widget/signin_with_google.dart
+++ lib/screens/signin/widget/signin_with_google.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pgapp/backend/authentication.dart';
 
 class SignInWithGoogle extends StatelessWidget {
   const SignInWithGoogle({
@@ -8,35 +9,41 @@ class SignInWithGoogle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.fromLTRB(30.0,5.0,30.0,5.0),
-      child: FlatButton(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(38),
-          side: BorderSide(
-            color: Colors.blue
-          )
-
-        ),
-        onPressed: () {},
-        child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-
-            Image.asset('assets/images/google_logo.png',height: 30,),
-            SizedBox(width: 20,),
-            Text('Sign in with Google',
-            style: TextStyle(
-              fontSize: 18,
-              color: Colors.blue),
-            textAlign: TextAlign.right,
-            ),
-
-          ]
-        ),
-        )
-        ),
-      );
+      margin: EdgeInsets.fromLTRB(30.0, 5.0, 30.0, 5.0),
+      child: TextButton(
+          style: TextButton.styleFrom(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(38),
+                side: BorderSide(color: Colors.blue)),
+          ),
+          onPressed: () async {
+            String check = await Authentication().signInWithGoogle();
+            if (check.length == 0)
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text("Account Created! You can now LogIn")));
+            else
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(SnackBar(content: Text("$check")));
+          },
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(
+                    'assets/images/google_logo.png',
+                    height: 30,
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text(
+                    'Sign in with Google',
+                    style: TextStyle(fontSize: 18, color: Colors.blue),
+                    textAlign: TextAlign.right,
+                  ),
+                ]),
+          )),
+    );
   }
 }
